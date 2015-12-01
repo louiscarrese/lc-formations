5ad54c93039379b0b3c9198aa638e1b961b9fc69
Louis Carrese <louis.carrese@gmail.com>
2015-12-01 23:15:30 +0100

    Add drop down type to myEditable directive

--- resources/assets/js/directives/myEditable-directive.js
+++ resources/assets/js/directives/myEditable-directive.js
@@ -4,18 +4,24 @@ function myEditableDirective() {
         scope: {
             type: '@',
             model: '=',
-            editingFlag: '='
+            editingFlag: '=',
+            source: '='
         },
         template: function(tElem, tAttr) {
             var template = '';
-            if(tAttr['type'] == 'text') {
+            if(tAttr['type'] === 'text') {
                 template += '<span ng-hide="editingFlag">{{model}}</span>';
                 template += '<input type="text" ng-show="editingFlag" ng-model="model" size="1" />';
-            } else if(tAttr['type'] == 'integer') {
+            } else if(tAttr['type'] === 'integer') {
                 template += '<span ng-hide="editingFlag">{{model}}</span>';
                 template += '<input type="text" ng-show="editingFlag" ng-model="model" size="1" my-force-integer/>';
-            } else if(tAttr['type'] == 'checkbox') {
+            } else if(tAttr['type'] === 'checkbox') {
                 template += '<input type="checkbox" ng-disabled="editingFlag" ng-model="model" />';
+            } else if(tAttr['type'] === 'dropdown') {
+                template += '<span ng-hide="editingFlag">{{model}}</span>';
+                template += '<select ng-show="editingFlag" ng-options="item.id as item.libelle for item in source" ng-model="model"></select>';
+            } else {
+                template += "Erreur de type !";
             }
             return template;
 
