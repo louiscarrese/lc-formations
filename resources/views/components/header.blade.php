8f3025ef5de1171db82d7633b8d27d21e71b7731
Louis Carrese <louis.carrese@gmail.com>
2015-12-01 23:17:05 +0100

    absolute uri in general links

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
     <div id="jm-logo"></div>
     <h1>Formations</h1>
     <div id="header-buttons">
-        <i class="material-icons clickable"><a href="parametres">settings</a></i>
-        <i class="material-icons clickable"><a href="logout">exit_to_app</a></i>
+        <i class="material-icons clickable"><a href="/parametres">settings</a></i>
+        <i class="material-icons clickable"><a href="/logout">exit_to_app</a></i>
     </div>
 </div>
