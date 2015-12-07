c32f5dbc0e55da83f83484b79fc52e7887081f8b
Louis Carrese <louis.carrese@gmail.com>
2015-12-07 10:57:51 +0100

    Added filtering in sub-objects

--- resources/assets/js/filters/myCustomFilter-filter.js
+++ resources/assets/js/filters/myCustomFilter-filter.js
@@ -7,16 +7,27 @@ function myCustomFilter() {
         for(elemId = 0; elemId < input.length; elemId++) {
             //Pour chaque champ à analyser
             for(fieldId = 2; fieldId < arguments.length; fieldId++) {
-                var stringValue = angular.lowercase('' + input[elemId][arguments[fieldId]]);
+                //On découpe, au cas où ça serait un sous objet (genre objet.propriete)
+                var fieldPath = arguments[fieldId].split('.');
+                //On démarre sur l'élément du tableau
+                var fieldValue = input[elemId];
+                //On descend chaque champ de l'arborescence
+                for(var i = 0; i < fieldPath.length; i++) {
+                    fieldValue = fieldValue[fieldPath[i]];
+                }
 
+                //On converti en string lowercase
+                var stringValue = angular.lowercase('' + fieldValue);
+
+                //On cherche le filtre dans la valeur
                 if(stringValue.indexOf(lowerFilter) !== -1) {
                     outArray.push(input[elemId]);
+                    //Si on l'a trouvé une fois, on peut passer à l'élément de tableau suivant
                     break;
                 }
             }
         }
         return outArray;
-
     }
 
 }
