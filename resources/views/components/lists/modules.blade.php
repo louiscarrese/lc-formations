d1c4aba5e5f14b06609733e334bfe317928421bb
Louis Carrese <louis.carrese@gmail.com>
2015-12-13 21:47:34 +0100

    Module and Session lists use the listTable blade component

--- resources/views/components/lists/modules.blade.php
+++ resources/views/components/lists/modules.blade.php
@@ -1,62 +1,27 @@
-<h2>Modules</h2>
+@include('components.listTable', 
+    [
+        'title' => 'Modules',
+        'controllerName' => 'modulesListController',
+        'detailUri' => '/modules',
+        'fields' => [
+            'id' => [
+                'isId' => true,
+                'label' => 'Id',
+                'sortable' => true,
+                'filterable' => true,
+                'tdClass' => 'centered'
+            ],
+            'libelle' => [
+                'label' => 'Libelle',
+                'sortable' => true,
+                'filterable' => true,
+            ],
+            'domaine_formation.id' => [
+                'label' => 'Domaine',
+                'sortable' => true,
+                'filterable' => true,
+                'displayedField' => 'domaine_formation.libelle',
+            ],
+        ]
+    ])
 
-<div ng-controller="modulesListController as modulesListCtrl" class="list-table-container">
-    <input type="text" ng-model="modulesListCtrl.filterInput" placeholder="Recherche locale" class="table-filter" />
-
-    <table>
-        <thead>
-            <tr>
-                <td class="clickable">
-                    <my-sortable-header set="modulesListCtrl.setSort('id')" get="modulesListCtrl.getSort('id')">Id</my-sortable-header>
-                </td>
-                <td class="clickable">
-                    <my-sortable-header set="modulesListCtrl.setSort('libelle')" get="modulesListCtrl.getSort('libelle')">Libellé</my-sortable-header>
-                </td>
-                <td class="clickable">
-                    <my-sortable-header set="modulesListCtrl.setSort('domaine_formation_id')" get="modulesListCtrl.getSort('domaine_formation_id')">Domaine</my-sortable-header>
-                </td>
-                <td>
-                    <!-- Details -->
-                </td>
-                <td>
-                    <!-- Edit -->
-                </td>
-            </tr>
-        </thead>
-        <tbody>
-            <tr ng-repeat="module in modulesListCtrl.data | myCustomFilter:modulesListCtrl.filterInput:'id':'libelle':'domaine_formation.libelle' track by module.id">
-                <td class="centered">
-                    <span>@{{module.id}}</span>
-                </td>
-                <td>
-                    <span>@{{module.libelle}}</span>
-                </td>
-                <td>
-                    <span>@{{module.domaine_formation.libelle}}</span>
-                </td>
-                <td>
-                    <span>
-                        <a ng-href="/modules/@{{module.id}}">
-                            <i class="icon clickable">info</i>
-                        </a>
-                    </span>
-                </td>
-                <td>
-                    <span>
-                        <a ng-href="/modules/@{{module.id}}?edit=true">
-                            <i class="icon clickable">edit</i>
-                        </a>
-                    </span>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-
-    <div class="global-actions">
-        <span>
-            <a ng-href="/modules/create">
-                <i class="icon clickable">add</i>
-            </a>
-        </span>
-    </div>
-</div>
--- resources/views/components/lists/sessions.blade.php
+++ resources/views/components/lists/sessions.blade.php
@@ -1,63 +1,21 @@
-<h2>Sessions</h2>
+@include('components.listTable', 
+    [
+        'title' => 'Sessions',
+        'controllerName' => 'sessionsListController',
+        'detailUri' => '/sessions',
+        'fields' => [
+            'id' => [
+                'isId' => true,
+                'label' => 'Id',
+                'sortable' => true,
+                'filterable' => true,
+                'tdClass' => 'centered'
+            ],
+            'libelle' => [
+                'label' => 'Libelle',
+                'sortable' => true,
+                'filterable' => true,
+            ],
+        ]
+    ])
 
-<div ng-controller="sessionsListController as sessionsListCtrl" class="list-table-container">
-    <input type="text" ng-model="sessionsListCtrl.filterInput" placeholder="Recherche locale" class="table-filter" />
-
-    <table>
-        <thead>
-            <tr>
-                <td class="clickable">
-                    <my-sortable-header set="sessionsListCtrl.setSort('id')" get="sessionsListCtrl.getSort('id')">Id</my-sortable-header>
-                </td>
-                <td class="clickable">
-                    <my-sortable-header set="sessionsListCtrl.setSort('libelle')" get="sessionsListCtrl.getSort('libelle')">Libellé</my-sortable-header>
-                </td>
-                <td class="clickable">
-                    <my-sortable-header set="sessionsListCtrl.setSort('domaine_formation_id')" get="sessionsListCtrl.getSort('domaine_formation_id')">Domaine</my-sortable-header>
-                </td>
-                <td>
-                    <!-- Details -->
-                </td>
-                <td>
-                    <!-- Edit -->
-                </td>
-            </tr>
-        </thead>
-        <tbody>
-            <tr ng-repeat="session in sessionsListCtrl.data | myCustomFilter:sessionsListCtrl.filterInput:'id':'libelle':'domaine_formation.libelle' track by session.id">
-                <td class="centered">
-                    <span>@{{session.id}}</span>
-                </td>
-                <td>
-                    <span>@{{session.libelle}}</span>
-                </td>
-                <td>
-                    <span>@{{session.domaine_formation.libelle}}</span>
-                </td>
-                <td>
-                    <span>
-                        <a ng-href="/sessions/@{{session.id}}">
-                            <i class="icon clickable">info</i>
-                        </a>
-                    </span>
-                </td>
-                <td>
-                    <span>
-                        <a ng-href="/sessions/@{{session.id}}?edit=true">
-                            <i class="icon clickable">edit</i>
-                        </a>
-                    </span>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-
-    <div class="global-actions">
-        <span>
-            <a ng-href="/sessions/create">
-                <i class="icon clickable">add</i>
-            </a>
-        </span>
-    </div>
-   <div class="error-message">@{{sessionsListCtrl.errorMessage}}</div>
-</div>
