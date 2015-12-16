dad4ad8e91d24b389e3718a0e568aa9aefc79a1d
Louis Carrese <louis.carrese@gmail.com>
2015-12-16 10:54:23 +0100

    Fixed wrong end tags in myEditable blade template

--- resources/views/components/myEditable.blade.php
+++ resources/views/components/myEditable.blade.php
@@ -1,13 +1,13 @@
 @if($field['type'] == 'text')
-    <my-editable-text ng-model="{{$element}}.{{$fieldId}}" editing-flag="{{$editingFlag}}" {{$field['additionalAttributes'] or ''}} {{$field['validation'] or ''}}></my-editable>
+    <my-editable-text ng-model="{{$element}}.{{$fieldId}}" editing-flag="{{$editingFlag}}" {{$field['additionalAttributes'] or ''}} {{$field['validation'] or ''}}></my-editable-text>
 @elseif($field['type'] == 'integer')
-    <my-editable-integer ng-model="{{$element}}.{{$fieldId}}" editing-flag="{{$editingFlag}}" {{$field['additionalAttributes'] or ''}} {{$field['validation'] or ''}}></my-editable>
+    <my-editable-integer ng-model="{{$element}}.{{$fieldId}}" editing-flag="{{$editingFlag}}" {{$field['additionalAttributes'] or ''}} {{$field['validation'] or ''}}></my-editable-integer>
 @elseif($field['type'] == 'radio')
-    <my-editable-radio ng-model="{{$element}}.{{$fieldId}}" editing-flag="{{$editingFlag}}" values="{{$field['values']}}" {{$field['additionalAttributes'] or ''}} {{$field['validation'] or ''}}></my-editable>
+    <my-editable-radio ng-model="{{$element}}.{{$fieldId}}" editing-flag="{{$editingFlag}}" values="{{$field['values']}}" {{$field['additionalAttributes'] or ''}} {{$field['validation'] or ''}}></my-editable-radio>
 @elseif($field['type'] == 'checkbox')
-    <my-editable-checkbox ng-model="{{$element}}.{{$fieldId}}" editing-flag="{{$editingFlag}}" {{$field['additionalAttributes'] or ''}} {{$field['validation'] or ''}}></my-editable>
+    <my-editable-checkbox ng-model="{{$element}}.{{$fieldId}}" editing-flag="{{$editingFlag}}" {{$field['additionalAttributes'] or ''}} {{$field['validation'] or ''}}></my-editable-checkbox>
 @elseif($field['type'] == 'textarea')
-    <my-editable-textarea ng-model="{{$element}}.{{$fieldId}}" editing-flag="{{$editingFlag}}" {{$field['additionalAttributes'] or ''}} {{$field['validation'] or ''}}></my-editable>
+    <my-editable-textarea ng-model="{{$element}}.{{$fieldId}}" editing-flag="{{$editingFlag}}" {{$field['additionalAttributes'] or ''}} {{$field['validation'] or ''}}></my-editable-textarea>
 @elseif($field['type'] == 'dropdown')
-    <my-editable-dropdown ng-model="{{$element}}.{{$fieldId}}" editing-flag="{{$editingFlag}}" source="{{$controllerName}}.{{$field['dropdownDatasource']}}" source-id="{{$field['dropdownDataId']}}" source-label="{{$field['dropdownDataLabel']}}" model-label="{{$element}}.{{$field['dropdownLabel']}}" change="{{$field['change'] or '' }}" {{$field['additionalAttributes'] or ''}}></my-editable>
+    <my-editable-dropdown ng-model="{{$element}}.{{$fieldId}}" editing-flag="{{$editingFlag}}" source="{{$controllerName}}.{{$field['dropdownDatasource']}}" source-id="{{$field['dropdownDataId']}}" source-label="{{$field['dropdownDataLabel']}}" model-label="{{$element}}.{{$field['dropdownLabel']}}" change="{{$field['change'] or '' }}" {{$field['additionalAttributes'] or ''}}></my-editable-dropdown>
 @endif
