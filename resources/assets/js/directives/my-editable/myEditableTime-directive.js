6358fce8c608a7b5fbe5e3feb63ad0894d6e4ae9
Louis Carrese <louis.carrese@gmail.com>
2015-12-18 18:18:23 +0100

    Removed a console.log

--- resources/assets/js/directives/my-editable/myEditableTime-directive.js
+++ resources/assets/js/directives/my-editable/myEditableTime-directive.js
@@ -1,6 +1,5 @@
 function myEditableDirectiveTime($filter) {
     function stringtoUTCTime(input) {
-        console.log('got : ' + input);
         var iso8601String = '1970-01-01T' + input;
 
         var time = new Date(iso8601String);
