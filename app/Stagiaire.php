4d181f3b29031f0ffe7aaa757db93c2f6b8208e9
Louis Carrese <louis.carrese@gmail.com>
2016-01-10 11:31:27 +0100

    Removed forgotten junk

--- app/Stagiaire.php
+++ app/Stagiaire.php
@@ -14,26 +14,6 @@ class Stagiaire extends AbstractModel
 
     protected static $myDates = ['demandeur_emploi_depuis', 'date_naissance'];
 
-            $stagiaire->id = $data['id'];
-        $stagiaire->nom = $data['nom'];
-        $stagiaire->prenom = $data['prenom'];
-        $stagiaire->sexe = $data['sexe'];
-        $stagiaire->date_naissance = $data['date_naissance'];
-        $stagiaire->adresse = $data['adresse'];
-        $stagiaire->code_postal = $data['code_postal'];
-        $stagiaire->ville = $data['ville'];
-        $stagiaire->tel_portable = $data['tel_portable'];
-        $stagiaire->tel_fixe = $data['tel_fixe'];
-        $stagiaire->email = $data['email'];
-        $stagiaire->profession = $data['profession'];
-        $stagiaire->etudes = $data['etudes'];
-        $stagiaire->demandeur_emploi_depuis = $data['demandeur_emploi_depuis'];
-        $stagiaire->niveau_qualification = $data['niveau_qualification'];
-        $stagiaire->domaine_pro = $data['domaine_pro'];
-
-        $stagiaire->employeur_id = $data['employeur_id'];
-        $stagiaire->stagiaire_type_id = $data['stagiaire_type_id'];
-
     function employeur() {
         return $this->belongsTo('ModuleFormation\Employeur');
     }
