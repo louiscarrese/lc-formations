6e925cbefc69f7937ece2ad6dfad0dd947b7d63d
Louis Carrese <louis.carrese@gmail.com>
2016-07-12 12:42:56 +0200

    Fixed home link in header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <div id="header" class="clearfix">
 <!--    <div id="jm-logo"></div> -->
-    <h1 class="pull-left"><a href="/">Formations</a></h1>
+    <h1 class="pull-left"><a href="/intra">Formations</a></h1>
     <div id="header-buttons" class="h1 pull-right">
         <a href="/intra/parametres"><span class="glyphicon glyphicon-cog"></span></a>
         <a href="/intra/logout"><span class="glyphicon glyphicon-log-out"</span></a>
