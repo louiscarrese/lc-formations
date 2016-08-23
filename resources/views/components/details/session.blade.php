dfd77516b490407c651a7e1b0a2979d19d0b4921
Louis Carrese <louis.carrese@gmail.com>
2016-08-23 17:22:12 +0200

    Printed documents open in new tab

--- resources/views/components/details/session.blade.php
+++ resources/views/components/details/session.blade.php
@@ -193,13 +193,13 @@
             <h3>Impressions</h3>
             <div class="custom-actions row">
                 <span class="col-sm-2">
-                    <a ng-href="{{$viewService->privateUrl('print/emargement/')}}@{{detailCtrl.data.id}}" class="btn btn-default">Feuilles d'émargement</a>
+                    <a ng-href="{{$viewService->privateUrl('print/emargement/')}}@{{detailCtrl.data.id}}" target="_blank" class="btn btn-default">Feuilles d'émargement</a>
                 </span>
                 <span class="col-sm-2">
-                    <a ng-href="{{$viewService->privateUrl('print/suivi_session/')}}@{{detailCtrl.data.id}}" class="btn btn-default">Feuille de suivi de session</a>
+                    <a ng-href="{{$viewService->privateUrl('print/suivi_session/')}}@{{detailCtrl.data.id}}" target="_blank" class="btn btn-default">Feuille de suivi de session</a>
                 </span>
                 <span class="col-sm-2">
-                    <a ng-href="{{$viewService->privateUrl('print/attestation/')}}@{{detailCtrl.data.id}}" class="btn btn-default">Attestations de fin de stage</a>
+                    <a ng-href="{{$viewService->privateUrl('print/attestation/')}}@{{detailCtrl.data.id}}" target="_blank" class="btn btn-default">Attestations de fin de stage</a>
                 </span>
             </div>
             <h3>Mails</h3>
