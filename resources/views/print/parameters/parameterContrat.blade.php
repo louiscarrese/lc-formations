41c8e4015409db126d3595a6edfb145b503c7afc
Louis Carrese <louis.carrese@gmail.com>
2016-09-01 22:15:42 +0200

    Arranged contrat parameters popin

--- resources/views/print/parameters/parameterContrat.blade.php
+++ resources/views/print/parameters/parameterContrat.blade.php
@@ -8,7 +8,7 @@
     </p>
     <p class="row">
         <label class="col-sm-3" for="adresse_stagiaire">Adresse du stagiaire : </label>
-        <input class="col-sm-6" type="text" name="adresse_stagiaire" value="{{$adresse_stagiaire}}" />
+        <input class="col-sm-5" type="text" name="adresse_stagiaire" value="{{$adresse_stagiaire}}" />
     </p>
     <p class="row">
         <label class="col-sm-3" for="libelle_module">Intitulé de la formation : </label>
@@ -16,11 +16,11 @@
     </p>
     <p class="row">
         <label class="col-sm-3" for="dates">Dates de la formation : </label>
-        <input class="col-sm-5" type="text" name="dates" value="{{$dates}}" />
+        <input class="col-sm-3" type="text" name="dates" value="{{$dates}}" />
     </p>
     <p class="row">
         <label class="col-sm-3" for="montant">Montant à payer : </label>
-        <input class="col-sm-2" type="text" name="montant" value="{{$montant}}" />
+        <input class="col-sm-1" type="text" name="montant" value="{{$montant}}" />
     </p>
     <div class="buttons row">
         <button type="submit" class="btn btn-default">En avant !</button>
