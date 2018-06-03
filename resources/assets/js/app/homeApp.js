f16f612e3e9cb2609c1f844febe6ac59f7133e2b
Louis Carrese <carrese.louis@gmail.com>
2018-06-03 15:44:30 +0200

    fix paramètres d'initialisation des controllers dans la home page

--- resources/assets/js/app/homeApp.js
+++ resources/assets/js/app/homeApp.js
@@ -2,10 +2,10 @@ angular.module('homeApp', ['ngResource', 'listTable'])
     .factory('sessionsService', ['$resource', sessionsServiceFactory])
     .factory('sessionsTableService', ['$filter', 'sharedDataService', sessionsTableServiceFactory])
 
-    .controller('sessionsListController', ['$filter', '$attrs', '$q', 'sessionsService', 'sessionsTableService', editableTableController])
+    .controller('sessionsListController', ['$scope', '$filter', '$attrs', '$q', 'sessionsService', 'sessionsTableService', editableTableController])
 
     .factory('inscriptionsService', ['$resource', inscriptionsServiceFactory])
     .factory('inscriptionsTableService', ['$filter', 'sharedDataService', inscriptionsTableServiceFactory])
 
-    .controller('inscriptionsListController', ['$filter', '$attrs', '$q', 'inscriptionsService', 'inscriptionsTableService', editableTableController])
+    .controller('inscriptionsListController', ['$scope', '$filter', '$attrs', '$q', 'inscriptionsService', 'inscriptionsTableService', editableTableController])
 ;
